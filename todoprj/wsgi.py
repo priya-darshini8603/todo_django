2a80d12a4854b0e87e90ed372e6b713770d6cdf4
priya-darshini8603 <darshini.devi8603@gmail.com>
2025-06-10 23:03:01 +0530

    Final Changes

--- todoprj/wsgi.py
+++ todoprj/wsgi.py
@@ -1,7 +1,7 @@
 """
 WSGI config for todoprj project.
 
-It exposes the WSGI callable as a module-level variable named ``app``.
+It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todoprj.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
