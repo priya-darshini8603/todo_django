fefc2f24a6bf712782954c701fc580591ea9161d
Priya Darshini I <darshini.devi8603@gmail.com>
2025-06-11 17:52:49 +0530

    Update urls.py

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',views.home,name='home-page'),
     path('register/',views.register,name='register'),
-    path('login/',views.loginpage,name='login'),
+    path('', views.loginpage, name='login'),
+    path('home/', views.home, name='home-page'),
     path('logout/', views.LogoutView, name='logout'),
     path('delete-task/<str:name>/', views.DeleteTask, name='delete'), #<str:name> delete particular item from database and pass to view function
     path('update/<str:name>/', views.Update, name='update'),
